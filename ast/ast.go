969870e8da4aeee3397e178033ce7f29fc8c6178
Georgi Vasilev <35624206+georgi-vasilev@users.noreply.github.com>
2024-08-23 13:48:10 +0300

    Add str implementation and extend parser tests (#6)

--- ast/ast.go
+++ ast/ast.go
@@ -33,6 +33,16 @@ func (p *Program) TokenLiteral() string {
     }
 }
 
+func (p* Program) String() string {
+    var out bytes.Buffer
+
+    for _, s := range p.Statements {
+        out.WriteString(s.String())
+    }
+
+    return out.String()
+}
+
 type LetStatement struct {
     Token token.Token
     Name  *Identifier
@@ -127,3 +137,25 @@ func (pe *PrefixExpression) String() string {
 
     return out.String()
 }
+
+type InfixExpression struct {
+    Token    token.Token
+    Left     Expression
+    Operator string
+    Right    Expression
+}
+
+func (oe *InfixExpression) expressionNode() {}
+func (oe *InfixExpression) TokenLiteral() string { return oe.Token.Literal }
+func (oe *InfixExpression) String() string {
+    var out bytes.Buffer
+
+    out.WriteString("(")
+    out.WriteString(oe.Left.String())
+    out.WriteString(" " + oe.Operator + " ")
+    out.WriteString(oe.Right.String())
+    out.WriteString(")")
+
+    return out.String()
+}
+
