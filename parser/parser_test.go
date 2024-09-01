f30cc0a21677cf08603001160702b48a80b44e35
Georgi Vasilev <georgisvasilevv@gmail.com>
2024-09-01 12:06:08 +0300

    Implement if expression parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -217,6 +217,7 @@ func TestParsingInfixExpressions(t *testing.T) {
 	}
 
 }
+
 func TestOperatorPrecedenceParsing(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -290,6 +291,26 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 			"3 < 5 == true",
 			"((3 < 5) == true)",
 		},
+		{
+			"1 + (2 + 3) + 4",
+			"((1 + (2 + 3)) + 4)",
+		},
+		{
+			"(5 + 5) * 2",
+			"((5 + 5) * 2)",
+		},
+		{
+			"2 / (5 + 5)",
+			"(2 / (5 + 5))",
+		},
+		{
+			"-(5 + 5)",
+			"(-(5 + 5))",
+		},
+		{
+			"!(true == true)",
+			"(!(true == true))",
+		},
 	}
 	for _, tt := range tests {
 		l := lexer.New(tt.input)
@@ -305,6 +326,49 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 	}
 }
 
+func TestIfExpression(t *testing.T) {
+    input := `if (x < y) { x }`
+
+    l := lexer.New(input)
+    p := New(l)
+    program := p.ParseProgram()
+    checkParserErrors(t, p)
+
+    if len(program.Statements) != 1 {
+        t.Fatalf("program.Body does not contain %d statements. got=%d\n",
+        1, len(program.Statements))
+    }
+
+    stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+    if !ok {
+        t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+        program.Statements[0])
+    }
+
+    exp, ok := stmt.Expression.(*ast.IfExpression)
+    if !ok {
+        t.Fatalf("stmt.Expression is not ast.IfExpression. got=%T",
+        stmt.Expression)
+    }
+    if !testInfixExpression(t, exp.Condition, "x", "<", "y") {
+        return
+    }
+    if len(exp.Consequence.Statements) != 1 {
+        t.Errorf("consequence is not 1 statements. got=%d\n",
+        len(exp.Consequence.Statements))
+    }
+    consequence, ok := exp.Consequence.Statements[0].(*ast.ExpressionStatement)
+    if !ok {
+        t.Fatalf("Statements[0] is not ast.ExpressionStatement. got=%T",
+        exp.Consequence.Statements[0])
+    }
+    if !testIdentifier(t, consequence.Expression, "x") {
+        return
+    }
+    if exp.Alternative != nil {
+        t.Errorf("exp.Alternative.Statements was not nil. got=%+v", exp.Alternative)
+    }
+}
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
 		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
@@ -398,7 +462,7 @@ func testLiteralExpression(
 	case int64:
 		return testIntegerLiteral(t, exp, v)
 	case string:
-		testIdentifier(t, exp, v)
+	  return testIdentifier(t, exp, v)
 	case bool:
 		return testBooleanLiteral(t, exp, v)
 	}
